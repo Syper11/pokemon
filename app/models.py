4564dcc8e3b4415b27675c8e9920da6e06a616d3
Syper11 <brettrickheim@gmail.com>
2023-01-20 15:48:48 -0600

    testing

--- app/models.py
+++ app/models.py
@@ -7,12 +7,16 @@ db = SQLAlchemy()
 # create models from out ERD
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False, unique=True)
+    lat_name = db.Column(db.String(50), nullable=False, unique=True)
     username = db.Column(db.String(45), nullable=False, unique=True)
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     
 
-    def __init__(self, username, email, password):
+    def __init__(self, first_name, last_name, username, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
         self.username = username
         self.email = email
         self.password = password
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,6 @@
 from app import app
-from flask import render_template, request, redirect, url_for
-from .forms import UserCreationForm
-from .models import User
+from flask import render_template
+
 
 @app.route('/')
 def homepage():
@@ -16,31 +15,3 @@ def friendly_battle():
 @app.route('/poke_selection')
 def apoke_selection():
     return render_template('poke_selection.html')
-
-
-@app.route('/signup', methods=["GET", "POST"])
-def signUpPage():
-    form = UserCreationForm()
-    print(request.method)
-    if request.method == 'POST':
-        if form.validate():
-            username = form.username.data
-            email = form.email.data
-            password = form.password.data
-            
-            print(username, email, password)
-
-            # add user to database
-            user = User(username, email, password)
-            print(user)
-
-            user.saveToDB()
-
-            return redirect(url_for('friendly_battle'))
-
-
-    return render_template('signup.html', form = form )
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
